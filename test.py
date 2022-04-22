23cb0c94438285c269d799c4d8e6ab7466b4929c
G-hackeyoei <103969348+G-hackeyoei@users.noreply.github.com>
2022-04-22 21:15:37 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+import random
+while True:
+    choice = input('【【口算】开平方根/立方根训练器】\n输入后会立刻开始计时，请小心使用\n训练开平方根输入2，训练开立方根输入3\n输入：')
+    if choice == '2':
+        num = random.randint(10, 99)
+        print('√' + str(num ** 2) + '=', end=' ')
+        res = int(input())
+        if res == num:
+            print('挑战成功！')
+        else:
+            print('挑战失败！')
+    if choice == '3':
+        num = random.randint(10, 99)
+        print('3√' + str(num ** 3) + '=', end=' ')
+        res = int(input())
+        if res == num:
+            print('挑战成功！')
+        else:
+            print('挑战失败！')
